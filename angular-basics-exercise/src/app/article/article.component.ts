fae273cfa69bd5115d4c29d23dcc022b5ca9dd06
kelbrad7 <98112140+kelbrad7@users.noreply.github.com>
2022-03-24 19:16:58 -0500

    12.7 exercise

---
+++ angular-basics-exercise/src/app/article/article.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.css']
+})
+export class ArticleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  title: string = "This is the title.";
+  content: string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+  isTechRelated: boolean = true;
+
+  changeColor() {this.isTechRelated = !this.isTechRelated};
+}
